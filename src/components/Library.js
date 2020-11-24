4d96a953168a11aada23e7646a4dfe2882d0f8d5
fearghalreilly <74102899+fearghalreilly@users.noreply.github.com>
2020-11-24 18:39:32 +0000

    Add files via upload

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import LibrarySong from './LibrarySong'
+import React from "react";
 
-const Library = ({songs}) => {
-    return(
-        <div className='library'>
-            <h2>Library</h2>
-            <div className='library-songs'>
-                {songs.map((song) => (
-                     <LibrarySong song={song} /> 
-                ))}
-            </div>
+import LibrarySong from "./LibrarySong";
 
-        </div>
+const Library = ({
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
+  return (
+    <div className={`library ${libraryStatus ? "active-library" : " "}`}>
+      <h2>Library</h2>
+      <div className="library-songs">
+        {songs.map((song) => (
+          <LibrarySong
+            songs={songs}
+            cover={song.cover}
+            name={song.name}
+            artist={song.artist}
+            active={song.active}
+            key={song.id}
+            id={song.id}
+            setCurrentSong={setCurrentSong}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+            setSongs={setSongs}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Library
+export default Library;
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Song = ({currrentSong}) => {
-    return(
-        <div className='song-container' >
-            <img alt={currrentSong.name} src={currrentSong.cover} />
-             <h2>{currrentSong.name}</h2>
-            <h3>{currrentSong.artist}</h3>
-        </div>
+const Song = ({ currentSong, isPlaying }) => {
+  return (
+    <div className="song-container">
+      <img
+        className={isPlaying ? "rotateSong" : ""}
+        src={currentSong.cover}
+        alt=""
+      />
+      <h2>{currentSong.name}</h2>
+      <h3>{currentSong.artist}</h3>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Song
+export default Song;
